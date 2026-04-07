4271d981b1673f3b29fe38a9228501916ad4870f
Karla Flores <karlaflrs_31@hotmail.com>
2026-04-07 15:28:27 -0600

    actividad 3

---
+++ app/src/main/java/com/example/a22698/actividad3/Navegacion.kt
@@ -0,0 +1,31 @@
+package com.example.a22698.actividad3
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
+
+///@Serializable
+///object Casa
+/*
+@Serializable
+object Edad
+
+@Serializable
+data class Calcular edad(val año: Int)
+
+@Preview(showBackground = true)
+@Composable
+fun Navegacion(){
+    val navegar = rememberNavController()
+    NavHost(navegar, startDestination = Casa){
+        composable <Casa>{  }
+
+        composable <Edad>{
+            val edades : Calcular = it.toRoute()
+        }
+    }
+}*/
